65fa8adcbbe6a72a8362aaba5fc18c22649111fe
Firad Fd <firadfd822@gmail.com>
2024-11-10 21:49:12 +0600

    scrollview basic

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -37,6 +38,157 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Container());
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),Container(
+                      margin: EdgeInsets.only(right: 8.0),
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
