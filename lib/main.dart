188a17263b17f3b221f422cfe10672eff27e6c74
Firad Fd <firadfd822@gmail.com>
2025-01-07 09:19:38 +0600

    custom widget practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -30,6 +32,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +42,134 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Container());
+        body: Column(
+            mainAxisAlignment:MainAxisAlignment.center,children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0,right: 18.0),
+            child: CustomTextInputField(
+              labelText: "Username",
+              hintText: "Enter your username",
+              controller: usernameController,
+              prefixIcon: const Icon(Icons.person),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 18.0,right: 18.0),
+            child: CustomTextInputField(
+              labelText: "Password",
+              hintText: "Enter your password",
+              controller: passwordController,
+              keyboardType: TextInputType.visiblePassword,
+              isPassword: true,
+              prefixIcon: const Icon(Icons.lock),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () {
+              print("Username: ${usernameController.text}");
+              print("Password: ${passwordController.text}");
+            },
+            child: const Text("Submit"),
+          ),
+        ]));
+  }
+}
+
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final void Function(String)? onPressed;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    this.color = Colors.black,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        if (onPressed != null) {
+          onPressed!(text);
+        }
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 18.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextInputField extends StatefulWidget {
+  final String labelText;
+  final String hintText;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final bool isPassword;
+  final Icon? prefixIcon;
+
+  const CustomTextInputField({
+    Key? key,
+    required this.labelText,
+    required this.hintText,
+    required this.controller,
+    this.keyboardType = TextInputType.text,
+    this.isPassword = false,
+    this.prefixIcon,
+  }) : super(key: key);
+
+  @override
+  _CustomTextInputFieldState createState() => _CustomTextInputFieldState();
+}
+
+class _CustomTextInputFieldState extends State<CustomTextInputField> {
+  bool _obscureText = true; // Controls whether the text is hidden or visible
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      obscureText: widget.isPassword && _obscureText,
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        prefixIcon: widget.prefixIcon,
+        filled: true,
+        fillColor: Colors.grey[200],
+        contentPadding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 18.0),
+        suffixIcon: widget.isPassword
+            ? IconButton(
+          icon: Icon(
+            _obscureText ? Icons.visibility_off : Icons.visibility,
+          ),
+          onPressed: () {
+            setState(() {
+              _obscureText = !_obscureText;
+            });
+          },
+        )
+            : null,
+      ),
+    );
   }
 }
